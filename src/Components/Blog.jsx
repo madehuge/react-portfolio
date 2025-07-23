b46c1fdfb94df700d8c585afa944ffccbb7c6216
Manishjangir <manishjangir027@gmail.com>
2025-07-23 11:41:02 +0530

    Portfolio Added

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import trueLeaderBlog from '../assets/images/blog/trueLeaderBlog.jpg';
+import collobrativeTeam from '../assets/images/blog/collobrativeTeam.jpg';
+import truthITHiring from '../assets/images/blog/truthITHiring.jpg';
 
 export default function Blog() {
   return (
@@ -7,7 +10,7 @@ export default function Blog() {
         <div className="container">
           <div className="main-heading mx-auto">
             <h2 className="main-head-animation">Blog</h2>
-            <span>Read News</span>
+            <span>Read About my Thoughts</span>
           </div>
           <div className="blog-field pt-4 row">
             {/* Blog Post 1 */}
@@ -22,14 +25,14 @@ export default function Blog() {
                 >
                   <img
                     className="img-fluid w-100 d-block"
-                    src="images/blog/blog1.jpg"
+                    src={trueLeaderBlog}
                     alt="blog image"
                   />
-                  <span className="blog-title">Web</span>
+                  <span className="blog-title">Leadership</span>
                 </a>
-                <h3 className="blog-head my-2 px-2">Web development technology</h3>
+                <h3 className="blog-head my-2 px-2">True Leader Thoughts</h3>
                 <p className="blog-text mb-3 px-2">
-                  The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz
+                  A true leader doesn’t just manage a team; they ensure fairness, empower juniors, and help them grow into strong professionals. Leadership is about fostering a culture where individuals are given opportunities to shine.
                 </p>
                 <hr className="my-0" />
                 <div className="blog-links d-flex justify-content-between align-items-center p-2">
@@ -38,7 +41,7 @@ export default function Blog() {
                     <a className="blog-comment" href="#">34</a>
                   </div>
                   <div className="more">
-                    <a className="show-pop" href="#" data-bs-toggle="modal" data-bs-target="#blogModal1">
+                    <a href="https://www.linkedin.com/feed/update/urn:li:activity:7299267873716256769/" target='_blank'>
                       Read More
                     </a>
                   </div>
@@ -57,14 +60,14 @@ export default function Blog() {
                 >
                   <img
                     className="img-fluid w-100 d-block"
-                    src="images/blog/blog2.jpg"
-                    alt="blog image"
+                    src={collobrativeTeam}
+                    alt="Collaborative Team"
                   />
-                  <span className="blog-title">Mobile</span>
+                  <span className="blog-title">Team Collaboration</span>
                 </a>
-                <h3 className="blog-head my-2 px-2">Flutter technology</h3>
+                <h3 className="blog-head my-2 px-2">Collaborative Team</h3>
                 <p className="blog-text mb-3 px-2">
-                  The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz
+                  True progress happens when we foster open discussions, embrace diverse perspectives, and support each other’s growth. Every insight, every experience, and every fresh idea contributes to shaping the future.
                 </p>
                 <hr className="my-0" />
                 <div className="blog-links d-flex justify-content-between align-items-center p-2">
@@ -73,7 +76,7 @@ export default function Blog() {
                     <a className="blog-comment" href="#">34</a>
                   </div>
                   <div className="more">
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#blogModal2">
+                    <a href="https://www.linkedin.com/feed/update/urn:li:activity:7296395380970110976/" target='_blank'>
                       Read More
                     </a>
                   </div>
@@ -92,14 +95,14 @@ export default function Blog() {
                 >
                   <img
                     className="img-fluid w-100 d-block"
-                    src="images/blog/blog3.jpg"
+                    src={truthITHiring}
                     alt="blog image"
                   />
-                  <span className="blog-title">Design</span>
+                  <span className="blog-title">Blog</span>
                 </a>
-                <h3 className="blog-head my-2 px-2">Attractive designs</h3>
+                <h3 className="blog-head my-2 px-2">Today’s IT Industry: Promises vs. Practice</h3>
                 <p className="blog-text mb-3 px-2">
-                  The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz
+                 What companies promise and what they practice often stand miles apart. Behind the glossy LinkedIn posts, flashy job descriptions, and vibrant career pages lies a growing culture of inconsistency and superficial branding.
                 </p>
                 <hr className="my-0" />
                 <div className="blog-links d-flex justify-content-between align-items-center p-2">
@@ -108,7 +111,7 @@ export default function Blog() {
                     <a className="blog-comment" href="#">34</a>
                   </div>
                   <div className="more">
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#blogModal3">
+                    <a href="https://www.linkedin.com/feed/update/urn:li:activity:7353648595151065089/" target='_blank'>
                       Read More
                     </a>
                   </div>
@@ -117,12 +120,12 @@ export default function Blog() {
             </div>
           </div>
 
-          {/* More Blogs Button */}
+          {/* More Blogs Button 
           <div className="more-blogs text-center mt-4 pt-2">
             <a href="#" className="main-btn">
               More Blogs
             </a>
-          </div>
+          </div>*/}
         </div>
       </section>
     </>
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import portfolio1 from '../assets/images/portfolio/portfolio1.jpg';
+import portfolio2 from '../assets/images/portfolio/portfolio2.jpg';
+import portfolio3 from '../assets/images/portfolio/portfolio3.jpg';
+import portfolio4 from '../assets/images/portfolio/portfolio4.jpg';
+import portfolio5 from '../assets/images/portfolio/portfolio5.jpg';
+import portfolio6 from '../assets/images/portfolio/portfolio6.jpg';
 
 export default function Portfolio() {
   return (
@@ -7,7 +13,7 @@ export default function Portfolio() {
         <div className="container">
           <div className="main-heading mx-auto">
             <h2 className="main-head-animation">Porfolio</h2>
-            <span>Our Projects</span>
+            <span>My Efforts</span>
           </div>
 
           <div className="prog-filter wow fadeIn">
@@ -26,9 +32,9 @@ export default function Portfolio() {
                 data-fancybox="gallery"
                 className="d-block position-relative"
                 data-caption="Design: Project Title"
-                href="images/portfolio/portfolio1.jpg"
+                href={portfolio1}
               >
-                <img className="img-fluid w-100 d-block" src="images/portfolio/portfolio1.jpg" alt="portfolio" />
+                <img className="img-fluid w-100 d-block" src={portfolio1} alt="portfolio" />
                 <div className="port-disc">
                   <span></span><span></span><span></span><span></span>
                   <i className="fas fa-search-plus"></i>
@@ -44,9 +50,9 @@ export default function Portfolio() {
                 data-fancybox="gallery"
                 data-caption="Development: Project Title"
                 className="d-block position-relative"
-                href="images/portfolio/portfolio2.jpg"
+                href={portfolio2}
               >
-                <img className="img-fluid w-100 d-block" src="images/portfolio/portfolio2.jpg" alt="portfolio" />
+                <img className="img-fluid w-100 d-block" src={portfolio2} alt="portfolio" />
                 <div className="port-disc">
                   <span></span><span></span><span></span><span></span>
                   <i className="fas fa-search-plus"></i>
@@ -62,9 +68,9 @@ export default function Portfolio() {
                 data-fancybox="gallery"
                 data-caption="Design: Project Title"
                 className="d-block position-relative"
-                href="images/portfolio/portfolio3.jpg"
+                href={portfolio3}
               >
-                <img className="img-fluid w-100 d-block" src="images/portfolio/portfolio3.jpg" alt="portfolio" />
+                <img className="img-fluid w-100 d-block" src={portfolio3} alt="portfolio" />
                 <div className="port-disc">
                   <span></span><span></span><span></span><span></span>
                   <i className="fas fa-search-plus"></i>
@@ -80,9 +86,9 @@ export default function Portfolio() {
                 data-fancybox="gallery"
                 data-caption="Marketing: Project Title"
                 className="d-block position-relative"
-                href="images/portfolio/portfolio4.jpg"
+                href={portfolio4}
               >
-                <img className="img-fluid w-100 d-block" src="images/portfolio/portfolio4.jpg" alt="portfolio" />
+                <img className="img-fluid w-100 d-block" src={portfolio4} alt="portfolio" />
                 <div className="port-disc">
                   <span></span><span></span><span></span><span></span>
                   <i className="fas fa-search-plus"></i>
@@ -98,9 +104,9 @@ export default function Portfolio() {
                 data-fancybox="gallery"
                 data-caption="Development: Project Title"
                 className="d-block position-relative"
-                href="images/portfolio/portfolio5.jpg"
+                href={portfolio5}
               >
-                <img className="img-fluid w-100 d-block" src="images/portfolio/portfolio5.jpg" alt="portfolio" />
+                <img className="img-fluid w-100 d-block" src={portfolio5} alt="portfolio" />
                 <div className="port-disc">
                   <span></span><span></span><span></span><span></span>
                   <i className="fas fa-search-plus"></i>
@@ -116,9 +122,9 @@ export default function Portfolio() {
                 data-fancybox="gallery"
                 data-caption="Marketing: Project Title"
                 className="d-block position-relative"
-                href="images/portfolio/portfolio6.jpg"
+                href={portfolio6}
               >
-                <img className="img-fluid w-100 d-block" src="images/portfolio/portfolio6.jpg" alt="portfolio" />
+                <img className="img-fluid w-100 d-block" src={portfolio6} alt="portfolio" />
                 <div className="port-disc">
                   <span></span><span></span><span></span><span></span>
                   <i className="fas fa-search-plus"></i>
@@ -129,9 +135,9 @@ export default function Portfolio() {
             </div>
           </div>
 
-          <div className="more-projects text-center mt-4 pt-2">
+          {/* <div className="more-projects text-center mt-4 pt-2">
             <a href="#" className="main-btn">More Projects</a>
-          </div>
+          </div> */}
         </div>
       </section>
     </>
